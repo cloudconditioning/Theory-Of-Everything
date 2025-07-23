9d96e3b8adfa177a70cc543c492aaa5e0ee3a6c1
cloudconditioning <222035948+cloudconditioning@users.noreply.github.com>
2025-07-23 14:41:21 -0400

    updated the variables in identity/

--- identity/variables.tf
+++ identity/variables.tf
@@ -1,33 +1,40 @@
 
 variable "identity_resource_group_name" {
   type = string
+  default = "rg-identity"
 }
 
 variable "identity_resource_group_location" {
   type = string
+  default = "EastUS2"
 }
 
 variable "federated_identity_prefix" {
   type        = string
   description = "Prefix of the Federated Identity Credential"
+  default = "gh-federated-identity"
 }
 
 variable "uami_name" {
   type        = string
   description = "Name of the User Assigned Managed Identity"
+  default = "GitHub-Managed-Identity"
 }
 
 variable "github_org_name" {
   type        = string
   description = "Name of the GitHub Organization"
+  default = "cloudconditioning"
 }
 
 variable "github_repo_name" {
   type        = string
   description = "Name of the GitHub Repository"
+  default = "Theory-Of-Everything"
 }
 
 variable "github_branches" {
   type = list(string) # Consider using a set for  unique branch names
+  default = [ "main", "dev" ]
 }
 
