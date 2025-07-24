29c37e2ab905f0e9787cff6978d52e24c95c54f6
cloudconditioning <222035948+cloudconditioning@users.noreply.github.com>
2025-07-24 10:29:19 -0400

    testing Enterprise CICD pipeline

--- enterprise/providers.tf
+++ enterprise/providers.tf
@@ -12,14 +12,16 @@ terraform {
     resource_group_name  = "rg-tfstate-enterprise"
     storage_account_name = "tfstateccj71qzqqm"
     container_name       = "tfstate"
-    key                  = "onprem.tfstate"
+    key                  = "enterprise.tfstate"
+    use_azuread_auth = true
     
   }
   
- 
 }
 
  provider "azurerm" {
     features{}
     # Set Subscription ID using the environment variable `ARM_SUBSCRIPTION_ID`
+    # export ARM_SUBSCRIPTION_ID=$(az account show --query id -o tsv) - Bash
+    # $env:ARM_SUBSCRIPTION_ID=$(az account show --query id -o
   }
